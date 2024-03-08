60948dba1af4e9db76ff20c6d3c32d5d9c8b3d0e
JorgeAyad <george.ayad1234@gmail.com>
2024-03-08 22:29:40 +1100

    trying to do part 1d (lights good button broken)

---
+++ Assignment1/S2/Section2a/Debug/Src/2a.d
@@ -0,0 +1,3 @@
+Src/2a.o: ../Src/2a.s ../Src/definitions.s ../Src/initialise.s
+../Src/definitions.s:
+../Src/initialise.s:
---
+++ Assignment1/S2/Section2b/Debug/Src/2b.d
@@ -0,0 +1,3 @@
+Src/2b.o: ../Src/2b.s ../Src/definitions.s ../Src/initialise.s
+../Src/definitions.s:
+../Src/initialise.s:
