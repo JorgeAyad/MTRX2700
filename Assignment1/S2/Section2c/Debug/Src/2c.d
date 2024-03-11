97e5ac08d755cad6dcc5e5987376360635c2a6fc
JorgeAyad <george.ayad1234@gmail.com>
2024-03-11 12:02:49 +1100

    part c in works
    This works once then doesn't go up again

---
+++ Assignment1/S2/Section2c/Debug/Src/2c.d
@@ -0,0 +1,3 @@
+Src/2c.o: ../Src/2c.s ../Src/definitions.s ../Src/initialise.s
+../Src/definitions.s:
+../Src/initialise.s:
---
+++ Assignment1/S2/Section2c/Debug/Src/subdir.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (11.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+S_SRCS += \
+../Src/2c.s \
+../Src/definitions.s \
+../Src/initialise.s 
+
+OBJS += \
+./Src/2c.o \
+./Src/definitions.o \
+./Src/initialise.o 
+
+S_DEPS += \
+./Src/2c.d \
+./Src/definitions.d \
+./Src/initialise.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/%.o: ../Src/%.s Src/subdir.mk
+	arm-none-eabi-gcc -mcpu=cortex-m4 -g3 -DDEBUG -c -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
+
+clean: clean-Src
+
+clean-Src:
+	-$(RM) ./Src/2c.d ./Src/2c.o ./Src/definitions.d ./Src/definitions.o ./Src/initialise.d ./Src/initialise.o
+
+.PHONY: clean-Src
+
