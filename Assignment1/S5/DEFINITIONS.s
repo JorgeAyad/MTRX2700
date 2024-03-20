ebda91cfa921c92922d14532c3dd1732f873a460
CaseTarsierTheFirst <160694517+CaseTarsierTheFirst@users.noreply.github.com>
2024-03-20 11:51:01 +1100

    Create DEFINITIONS.s
    added definitions.s

---
+++ Assignment1/S5/DEFINITIONS.s
@@ -0,0 +1,11 @@
+@ For the Transmit and Encode
+
+@Alphabet ASCII Definitions
+
+.equ BUFFER_SIZE, 32
+.equ ASCII_UPPERCASE_A, 65 // Defining Variables for the ASCII bounds
+.equ ASCII_UPPERCASE_Z, 90
+.equ ASCII_LOWERCASE_A, 97
+.equ ASCII_LOWERCASE_Z, 122
+.equ CONVERT_DIFFERENCE, 0
+
